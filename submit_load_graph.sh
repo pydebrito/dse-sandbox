b87d8e64cc6f4fb528f423d850dd386d70ddb147
Pierre-Yves de Brito <pydebrito@gmail.com>
2019-02-25 16:27:52 +0100

    cleaner loader

--- submit_load_graph.sh
+++ submit_load_graph.sh
@@ -1,1 +1,2 @@
-dse spark-submit --conf spark.executor.memory=2g --conf spark.cassandra.output.concurrent.writes=1 load_graph.py
+#dse spark-submit --conf spark.executor.memory=2g --conf spark.cassandra.output.concurrent.writes=1 load_graph.py
+dse spark-submit --conf spark.executor.memory=2g --conf spark.cassandra.output.concurrent.writes=1 load_graph_cleaner.py
