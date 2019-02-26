310a058f4f49996b1a588a0722a4b1a642f65b04
Pierre-Yves de Brito <pydebrito@gmail.com>
2019-02-26 13:39:48 +0100

    100 concurrent writes -> 28 min instead of 1:20

--- submit_load_graph.sh
+++ submit_load_graph.sh
@@ -1,2 +1,2 @@
 #dse spark-submit --conf spark.executor.memory=2g --conf spark.cassandra.output.concurrent.writes=1 load_graph.py
-dse spark-submit --conf spark.executor.memory=2g --conf spark.cassandra.output.concurrent.writes=1 load_graph_cleaner.py
+dse spark-submit --conf spark.executor.memory=2g --conf spark.cassandra.output.concurrent.writes=100 load_graph_cleaner.py
